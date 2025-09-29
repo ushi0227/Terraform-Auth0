08453504aca3da7174f2cbb8f0a6e5ae3f5125d5
ushi0227 <toru.ushida@lac.co.jp>
2025-09-29 14:46:01 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ resource "auth0_client" "sample" {
   web_origins         = ["http://localhost:3000"]
 }
 
-# resource "auth0_user" "user" {
-#   connection_name = "Username-Password-Authentication"
-#   user_id         = "12345"
-#   username        = "demo_test"
-#   name            = "Test Demo"
-#   email           = "test.demo@lac.dev.local"
-#   email_verified  = true
-#   password        = "61LACushida"
-# }
+resource "auth0_user" "user" {
+  connection_name = "Username-Password-Authentication"
+  user_id         = "12345"
+  username        = "demo_test"
+  name            = "Test Demo"
+  email           = "test.demo@lac.dev.local"
+  email_verified  = true
+  password        = "61LACushida"
+}
